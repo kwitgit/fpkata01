0389f44bedf7d7eb079292b94d4f110e006c826d
Kent Withrow <kwithrow@gmail.com>
2019-01-21 00:02:31 -0500

    Added some preliminary types and started fooling around with text parsing.

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, rawText, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Parser exposing ((|.), (|=), Parser, spaces, succeed, symbol)
+
 
 
 ---- MODEL ----
@@ -15,6 +17,58 @@ type alias Model =
 init : ( Model, Cmd Msg )
 init =
     ( {}, Cmd.none )
+
+
+rawText : String
+rawText =
+    "       _  _     _  _  _  _  _\n     | _| _||_||_ |_   ||_||_|\n     ||_  _|  | _||_|  ||_| _|\n\n"
+
+
+type Bar
+    = LitBar
+    | EmptyBar
+
+
+type alias NumberFromBars =
+    List Bar
+
+
+validOne : NumberFromBars
+validOne =
+    [ EmptyBar, EmptyBar, EmptyBar, EmptyBar, EmptyBar, LitBar, EmptyBar, EmptyBar, LitBar ]
+
+
+validTwo : NumberFromBars
+validTwo =
+    [ EmptyBar, LitBar, EmptyBar, LitBar, LitBar, LitBar, LitBar, LitBar, EmptyBar ]
+
+
+validThree : NumberFromBars
+validThree =
+    [ EmptyBar, LitBar, EmptyBar, EmptyBar, LitBar, LitBar, EmptyBar, LitBar, LitBar ]
+
+
+
+-- TODO: more valid digits, through nine, and don't forget zero
+
+
+convertLineOfText : String -> List (Maybe NumberFromBars)
+convertLineOfText text =
+    [ Just validOne ]
+
+
+thingTest : Parser Bar
+thingTest =
+    succeed LitBar
+        |. symbol " "
+        |. symbol "_"
+        |. symbol " "
+        |. symbol "|"
+        |. symbol "_"
+        |. symbol "|"
+        |. symbol "|"
+        |. symbol "_"
+        |. symbol "|"
 
 
 
